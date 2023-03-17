318599924dffdc34ffa4666c868bdb505c6c3b85
user25 <user25@user25.ru>
2023-03-17 13:31:23 +0500

    Добавьте файлы проекта.

---
+++ DentistryWpfApp/Model/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentistryWpfApp.Model
+{
+    public class Core
+    {
+      public  Dentistry_ARMEntities context= new Dentistry_ARMEntities();
+    }
+}
---
+++ DentistryWpfApp/View/Pages/AutorizationPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DentistryWpfApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DentistryWpfApp.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorizationPage.xaml
+    /// </summary>
+    public partial class AutorizationPage : Page
+    {
+        public AutorizationPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    Core db= new Core();
+        private void AuthorizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (db.context.Personal.Where(x => x.Personal_login == AuthorizationTextBox.Text && x.Personal_Password == AuthPasswordBox.Password).FirstOrDefault() != null)
+            {
+                this.NavigationService.Navigate(new TestPage());
+            }
+            else
+            {
+                MessageBox.Show("Введены неверные данные!");
+            }
+        }
+    }
+}

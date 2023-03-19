c240fe7c38054a1d6fa8fb51de083065fe7e3621
Влуд Губарев <lolgagr@gmail.com>
2023-03-19 22:29:50 +0500

    Решена проблема со скрытием Grid.Row

--- DentistryWpfApp/MainWindow.xaml.cs
+++ DentistryWpfApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DentistryWpfApp.View.Pages;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +25,23 @@ namespace DentistryWpfApp
         public MainWindow()
         {
             InitializeComponent();
-            if(!MainFrame.CanGoBack) { 
-            NavigationStackPanel.Visibility = Visibility.Hidden;
-                
+            if(MainFrame.CanGoBack) {
+                UIGridRow.Height = new GridLength(50, GridUnitType.Pixel); 
             }
             MainFrame.Navigate(new AutorizationPage());
+        }
+        [ValueConversion(typeof(bool), typeof(GridLength))]
+        public class BoolToGridRowHeightConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return ((bool)value == true) ? new GridLength(1, GridUnitType.Star) : new GridLength(0);
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {    // Don't need any convert back
+                return null;
+            }
         }
     }
 }

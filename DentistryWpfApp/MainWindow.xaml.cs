c6a48d59a5c6143fcb69c05b5c331865ca675baf
Влуд Губарев <lolgagr@gmail.com>
2023-03-19 21:45:10 +0500

    Закончить Row

--- DentistryWpfApp/MainWindow.xaml.cs
+++ DentistryWpfApp/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace DentistryWpfApp
             InitializeComponent();
             if(!MainFrame.CanGoBack) { 
             NavigationStackPanel.Visibility = Visibility.Hidden;
+                UIGridRow.ActualHeight = 0;
             }
             MainFrame.Navigate(new AutorizationPage());
         }

3582bf56ed6e9e8c8f866584d729b5902772911f
Влуд Губарев <lolgagr@gmail.com>
2023-05-20 13:23:11 +0500

    пока работает

--- DentistryWpfApp/View/Pages/ResultPage.xaml.cs
+++ DentistryWpfApp/View/Pages/ResultPage.xaml.cs
@@ -8,13 +8,15 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using DentistryClassLibrary;
+using Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
 
 namespace DentistryWpfApp.View.Pages
 {
     /// <summary>
     /// Логика взаимодействия для ResultPage.xaml
     /// </summary>
-    public partial class ResultPage : Page
+    public partial class ResultPage : System.Windows.Controls.Page
     {
         int idGet;
         Core db = new Core();
@@ -98,11 +100,13 @@ namespace DentistryWpfApp.View.Pages
 
 
 
-        private void AddToWordButton_Click(object sender, RoutedEventArgs e)
+       
+
+private void AddToWordButton_Click(object sender, RoutedEventArgs e)
         {
             ContractGeneratorClass contractGeneratorClass = new ContractGeneratorClass();
 
-            // Открываем документ Word
+            // Открываем исходный документ Word
             string fileName = "043у.docx";
             string filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\Assets\\Documents\\", fileName);
 
@@ -110,199 +114,168 @@ namespace DentistryWpfApp.View.Pages
             string tempFilePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), fileName);
             System.IO.File.Copy(filePath, tempFilePath, true);
 
-            // Создаем объект Word и открываем документ
+            // Создаем объект Word и открываем временную копию документа
             Microsoft.Office.Interop.Word.Application word = null;
-            Microsoft.Office.Interop.Word.Document doc = null;
+            Microsoft.Office.Interop.Word.Document tempDoc = null;
             var client = db.context.Clients.FirstOrDefault(x => x.Clients_Id == idGet);
             try
             {
                 word = new Microsoft.Office.Interop.Word.Application();
-                doc = word.Documents.Open(tempFilePath, ReadOnly: false, Visible: true);
-                string valueToInsert1="";
-                // Ваш остальной код
-                // Получаем значения для вставки и задаем стиль "нижнее подчеркивание"
-                var personal = db.context.Personal.FirstOrDefault(x => x.Personal_Id == client.Personal_Id_FK);
-                if (personal != null)
-                {
-                    valueToInsert1 = personal.Personal_LastName + " " + personal.Personal_Name;
-                    // Вы можете использовать переменную "valueToInsert1" по вашему усмотрению
-                }
-              
-                Microsoft.Office.Interop.Word.Range bookmark1 = doc.Bookmarks["ЛечащийВрач"].Range;
-                bookmark1.Text = valueToInsert1;
-                bookmark1.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                tempDoc = word.Documents.Open(tempFilePath, ReadOnly: false, Visible: true);
 
-                string valueToInsert2 = "Лечащий драч";
-                Microsoft.Office.Interop.Word.Range bookmark2 = doc.Bookmarks["ДанныеРентгеновскихИсследований"].Range;
-                bookmark2.Text = valueToInsert2;
-                bookmark2.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                // Задаем стиль "Times New Roman, 14 кеглей" для текстовых элементов
+                Font font = tempDoc.Content.Font;
+                font.Name = "Times New Roman";
+              
+
+                ParagraphFormat paragraphFormat = tempDoc.Content.ParagraphFormat;
+                paragraphFormat.SpaceAfter = 0;
+                paragraphFormat.SpaceBefore = 0;
+
+                // Вставляем значения в соответствующие закладки
+                Personal personal = db.context.Personal.FirstOrDefault(x => x.Personal_Id == client.Personal_Id_FK);
+                // Закладка "ЛечащийВрач"
+                string valueToInsert1 = $"{personal.Personal_LastName} {personal.Personal_Name} {personal.Personal_Surname}";
+                Range bookmark1 = tempDoc.Bookmarks["ЛечащийВрач"].Range;
+                bookmark1.Text = valueToInsert1;
+                bookmark1.Underline = WdUnderline.wdUnderlineSingle;
 
                 string name = db.context.Clients.Where(x => x.Clients_Id == idGet).Select(x => x.Clients_Name).FirstOrDefault();
                 string surname = db.context.Clients.Where(x => x.Clients_Id == idGet).Select(x => x.Clients_Surname).FirstOrDefault();
                 string lastname = db.context.Clients.Where(x => x.Clients_Id == idGet).Select(x => x.Clients_Lastname).FirstOrDefault();
                 string valueToInsert3 = $"{lastname} {name} {surname}";
-                Microsoft.Office.Interop.Word.Range bookmark3 = doc.Bookmarks["ФИО"].Range;
+                Range bookmark3 = tempDoc.Bookmarks["ФИО"].Range;
                 bookmark3.Text = valueToInsert3;
-                bookmark3.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                bookmark3.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert4 = $"{DateTime.Now:yy}";
-                Microsoft.Office.Interop.Word.Range bookmark4 = doc.Bookmarks["ГодДвеПоследние"].Range;
+                Range bookmark4 = tempDoc.Bookmarks["ГодДвеПоследние"].Range;
                 bookmark4.Text = valueToInsert4;
-                bookmark4.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                bookmark4.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert5 = $"{DateTime.Now.Day}";
-                Microsoft.Office.Interop.Word.Range bookmark5 = doc.Bookmarks["Число"].Range;
+                Range bookmark5 = tempDoc.Bookmarks["Число"].Range;
                 bookmark5.Text = valueToInsert5;
-                bookmark5.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
-
-
-
+                bookmark5.Underline = WdUnderline.wdUnderlineSingle;
 
                 DateTime now = DateTime.Now;
                 string monthName = now.ToString("MMMM", CultureInfo.CreateSpecificCulture("ru-RU"));
 
                 string[] monthCases = new string[] { "января", "февраля", "марта", "апреля", "мая", "июня",
-                                     "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+                                             "июля", "августа", "сентября", "октября", "ноября", "декабря" };
 
                 int monthNumber = now.Month;
                 string inflectedMonthName = monthCases[monthNumber - 1];
 
-                string result = inflectedMonthName;
-                Console.WriteLine(result);
-
-                string valueToInsert6 = result;
+                string valueToInsert6 = inflectedMonthName;
+                Range bookmark6 = tempDoc.Bookmarks["Месяц"].Range;
+                bookmark6.Text = valueToInsert6;
+                bookmark6.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert7 = contractGenerator.GetNextContractNumber();
-                Microsoft.Office.Interop.Word.Range bookmark7 = doc.Bookmarks["Номер"].Range;
+                Range bookmark7 = tempDoc.Bookmarks["Номер"].Range;
                 bookmark7.Text = valueToInsert7;
-                bookmark7.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                bookmark7.Underline = WdUnderline.wdUnderlineSingle;
 
-
-                Microsoft.Office.Interop.Word.Range bookmark6 = doc.Bookmarks["Месяц"].Range;
-                bookmark6.Text = valueToInsert6;
-                bookmark6.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
-
-
-
-                var gender = db.context.Clients.Where(x=>x.Clients_Id==idGet).Select(x=>x.Clients_Gender).FirstOrDefault();
+                var gender = db.context.Clients.Where(x => x.Clients_Id == idGet).Select(x => x.Clients_Gender).FirstOrDefault();
                 string valueToInsert8 = gender;
-                Microsoft.Office.Interop.Word.Range bookmark8 = doc.Bookmarks["Пол"].Range;
-                
+                Range bookmark8 = tempDoc.Bookmarks["Пол"].Range;
                 bookmark8.Text = valueToInsert8;
-                bookmark8.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                bookmark8.Underline = WdUnderline.wdUnderlineSingle;
 
                 var adress = db.context.Clients.Where(x => x.Clients_Id == idGet).Select(x => x.Clients_Adress).FirstOrDefault();
                 string valueToInsert9 = adress;
-                Microsoft.Office.Interop.Word.Range bookmark9 = doc.Bookmarks["Адрес"].Range;
-
+                Range bookmark9 = tempDoc.Bookmarks["Адрес"].Range;
                 bookmark9.Text = valueToInsert9;
-                bookmark9.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
-                
-                int age=0;
+                bookmark9.Underline = WdUnderline.wdUnderlineSingle;
+
+                int age = 0;
                 if (client != null)
                 {
                     DateTime birthDate = (DateTime)client.Clients_Date;
                     DateTime currentDate = DateTime.Now;
-                     age = currentDate.Year - birthDate.Year;
+                    age = currentDate.Year - birthDate.Year;
 
-                    // Учтите случай, если текущая дата еще не достигла дня рождения клиента в текущем году
                     if (currentDate.Month < birthDate.Month || (currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
                     {
                         age--;
                     }
-
-                    // Теперь переменная "age" содержит возраст клиента
-                    // Вы можете использовать эту переменную по вашему усмотрению
                 }
-                
+
                 string valueToInsert10 = age.ToString();
-                Microsoft.Office.Interop.Word.Range bookmark10 = doc.Bookmarks["Возраст"].Range;
-
+                Range bookmark10 = tempDoc.Bookmarks["Возраст"].Range;
                 bookmark10.Text = valueToInsert10;
-                bookmark10.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
-
+                bookmark10.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert11 = client.Clients_Prof;
-                Microsoft.Office.Interop.Word.Range bookmark11 = doc.Bookmarks["Профессия"].Range;
-
+                Range bookmark11 = tempDoc.Bookmarks["Профессия"].Range;
                 bookmark11.Text = valueToInsert11;
-                bookmark11.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
-
+                bookmark11.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert12 = TransferredDiseasesTextBox.Text;
-                Microsoft.Office.Interop.Word.Range bookmark12 = doc.Bookmarks["СопутствующиеЗаблоевания"].Range;
-
+                Range bookmark12 = tempDoc.Bookmarks["СопутствующиеЗаблоевания"].Range;
                 bookmark12.Text = valueToInsert12;
-                bookmark12.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
-
+                bookmark12.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert13 = СomplaintsTextBox.Text;
-                Microsoft.Office.Interop.Word.Range bookmark13 = doc.Bookmarks["Жалобы"].Range;
-
+                Range bookmark13 = tempDoc.Bookmarks["Жалобы"].Range;
                 bookmark13.Text = valueToInsert13;
-                bookmark13.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                bookmark13.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert14 = RealDiseaseTextBox.Text;
-                Microsoft.Office.Interop.Word.Range bookmark14 = doc.Bookmarks["РазвитиеНастоящегоЗаболевания"].Range;
-
+                Range bookmark14 = tempDoc.Bookmarks["РазвитиеНастоящегоЗаболевания"].Range;
                 bookmark14.Text = valueToInsert14;
-                bookmark14.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                bookmark14.Underline = WdUnderline.wdUnderlineSingle;
 
                 string valueToInsert15 = MucousMembraneTextBox.Text;
-                Microsoft.Office.Interop.Word.Range bookmark15 = doc.Bookmarks["СостояниеСлизистой"].Range;
-
+                Range bookmark15 = tempDoc.Bookmarks["СостояниеСлизистой"].Range;
                 bookmark15.Text = valueToInsert15;
-                bookmark15.Underline = Microsoft.Office.Interop.Word.WdUnderline.wdUnderlineSingle;
+                bookmark15.Underline = WdUnderline.wdUnderlineSingle;
 
+                string valueToInsert16 = RentgenTextBox.Text;
+                Range bookmark16 = tempDoc.Bookmarks["ДанныеРентгеновскихИсследований"].Range;
+                bookmark16.Text = valueToInsert16;
+                bookmark16.Underline = WdUnderline.wdUnderlineSingle;
 
+                string valueToInsert17 = ResultTextBox.Text;
+                Range bookmark17 = tempDoc.Bookmarks["Эпикриз"].Range;
+                bookmark17.Text = valueToInsert17;
+                bookmark17.Underline = WdUnderline.wdUnderlineSingle;
 
-                // Показываем документ Word пользователю
-                word.Visible = true;
-                // Закрываем документ и освобождаем ресурсы
-                doc.Close();
-                word.Quit();
+                string valueToInsert18 = SovetTextBox.Text;
+                Range bookmark18 = tempDoc.Bookmarks["Наставления"].Range;
+                bookmark18.Text = valueToInsert18;
+                bookmark18.Underline = WdUnderline.wdUnderlineSingle;
+
+                // Сохраняем временную копию файла с данными
+                tempDoc.Save();
+
+                // Открываем временный файл в программе по умолчанию
+                System.Diagnostics.Process.Start(tempFilePath);
+
+                MessageBox.Show("Значения успешно добавлены в Word!");
             }
             finally
             {
-                if (doc != null)
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(doc);
+                if (tempDoc != null)
+                    Marshal.ReleaseComObject(tempDoc);
                 if (word != null)
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(word);
+                    Marshal.ReleaseComObject(word);
 
-                doc = null;
+                tempDoc = null;
                 word = null;
                 GC.Collect();
             }
-
-            // Открываем временный файл в программе по умолчанию
-            System.Diagnostics.Process.Start(tempFilePath);
-
-            MessageBox.Show("Значения успешно добавлены в Word!");
         }
 
-        private void NextButton_Click(object sender, RoutedEventArgs e)
+
+
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            // Переключаем видимость разделов
-
-            if (Section1.Visibility == Visibility.Visible)
-            {
-                // Если отображается раздел 1, переключаем на раздел 2
-                Section1.Visibility = Visibility.Collapsed;
-                Section2.Visibility = Visibility.Visible;
-                NextButton.Content = "Сохранить";
-                NextButton.Click -= NextButton_Click;
-                NextButton.Click += AddToWordButton_Click;
-
-            }
-            else if (Section2.Visibility == Visibility.Visible)
-            {
-                // Если отображается раздел 2, выполните необходимые действия
-                // и переключите обратно на раздел 1 или скройте кнопку "Далее"
-                // в зависимости от ваших требований
-                
-                NextButton.Content = "Сохранить";
-                
-            }
+            // Переходите на предыдущую страницу
+            Section1.Visibility = Visibility.Visible;
+            Section2.Visibility = Visibility.Collapsed ;
         }
 
         private void SaveData()
@@ -312,12 +285,42 @@ namespace DentistryWpfApp.View.Pages
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            // Обработчик событий для кнопки "Сохранить"
-            SaveData();
+            // Здесь можно выполнить необходимые действия для сохранения данных
 
-            // Скрываем кнопку "Далее"
-            NextButton.Visibility = Visibility.Collapsed;
+            // Проверяем, находимся ли мы на последней странице (разделе)
+            if (Section2.Visibility == Visibility.Visible)
+            {
+                // Если да, то сохраняем данные и скрываем кнопку "Далее"
+                SaveData();
+                NextButton.Visibility = Visibility.Collapsed;
+            }
         }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Section1.Visibility == Visibility.Visible)
+            {
+                // Если отображается раздел 1, переключаем на раздел 2
+                Section1.Visibility = Visibility.Collapsed;
+                Section2.Visibility = Visibility.Visible;
+                BackButton.Visibility = Visibility.Visible;
+                NextButton.Content = "Сохранить";
+                NextButton.Click -= NextButton_Click;
+                NextButton.Click += AddToWordButton_Click;
+
+                // Скрываем кнопку "Сохранить" на первом разделе
+              
+            }
+            else if (Section2.Visibility == Visibility.Visible)
+            {
+                // Если отображается раздел 2, выполните необходимые действия
+                // и переключите обратно на раздел 1 или скройте кнопку "Далее"
+                // в зависимости от ваших требований
+
+                NextButton.Content = "Сохранить";
+            }
+        }
+
     }
 }
 

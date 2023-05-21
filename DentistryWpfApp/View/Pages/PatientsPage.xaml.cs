eed30877da2c30c74b840f236c56348986bbc9dd
Влуд Губарев <lolgagr@gmail.com>
2023-05-21 21:56:02 +0500

    Pre-Release

--- DentistryWpfApp/View/Pages/PatientsPage.xaml.cs
+++ DentistryWpfApp/View/Pages/PatientsPage.xaml.cs
@@ -1,113 +1,116 @@
 ﻿using DentistryWpfApp.Model;
-using JetBrains.Annotations;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Web;
-using System.Net;
-using System.Net.Mail;
-using System.Net.Mime;
 using DentistryWpfApp.View.Windows;
 
 namespace DentistryWpfApp.View.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для PatientsPage.xaml
-    /// </summary>
     public partial class PatientsPage : Page
     {
-
-
+        private readonly Core db;
+        private int itemsPerPage = 10;
 
         public PatientsPage()
         {
-            Core db = new Core();
+            db = new Core();
             InitializeComponent();
-
-            int itemsPerPage = 10; // количество записей на одной странице
-            int totalItems = db.context.Clients.Count(); // общее количество записей
-            int totalPages = (int)Math.Ceiling((double)totalItems / itemsPerPage); // общее количество страниц
-
-            for (int i = 1; i <= totalPages; i++)
-            {
-                Button pageButton = new Button();
-                pageButton.Content = i.ToString();
-                pageButton.Tag = i; // сохраняем номер страницы в Tag кнопки
-                pageButton.Click += PageButton_Click; // подписываемся на событие Click
-                pageButton.Margin = new Thickness(0, 0, 10, 0);
-                paginationStackPanel.Children.Add(pageButton); // добавляем кнопку на панель
-            }
-
-            LoadPage(1); // загружаем записи для первой страницы
-
-            void PageButton_Click(object sender, RoutedEventArgs e)
-            {
-                Button button = (Button)sender;
-                int pageNumber = (int)button.Tag;
-                LoadPage(pageNumber); // загружаем записи для выбранной страницы
-            }
-
-            void LoadPage(int pageNumber)
-            {
-                // определяем номер первой записи на странице
-                int firstItemIndex = (pageNumber - 1) * itemsPerPage;
-
-                // загружаем записи для текущей страницы
-                var clients = db.context.Clients
-                    .OrderBy(x => x.Clients_Lastname)
-                    .Skip(firstItemIndex)
-                    .Take(itemsPerPage)
-                    .ToList();
-
-                // очищаем контейнер со списком пациентов
-                MainPatientsStackPanel.Children.Clear();
-
-                // отображаем записи на странице
-                foreach (var client in clients)
-                {
-                    Button button = new Button
-                    {
-                        Name = $"Button{client.Clients_Id}",
-                        Content = $" №{client.Clients_Id}. {client.Clients_Lastname} {client.Clients_Name}",
-                        Style = this.FindResource("PatientsButton") as Style
-                    };
-                    button.Margin = new Thickness(0, 10, 0, 0);
-                    button.Click += Button_Click;
-                   
-                    MainPatientsStackPanel.Children.Add(button);
-                }
-            }
-
-            void Button_Click(object sender, RoutedEventArgs e)
-            {
-                Button button = sender as Button;
-                
-                int id = int.Parse(button.Name.Substring(6));
-                this.NavigationService.Navigate(new ViewingPatientPage(id));
-            }
+            LoadPage(1);
+            AddPageButtons();
         }
-
-
-       
-
-      
 
         private void AddButtonClick(object sender, RoutedEventArgs e)
         {
             AddPatientWindow win2 = new AddPatientWindow();
             win2.Show();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchQuery = SearchTextBox.Text.Trim();
+            LoadPage(1, searchQuery);
+            ClearPagination();
+            AddPageButtons(searchQuery);
+        }
+
+        private void LoadPage(int pageNumber, string searchQuery = "")
+        {
+            int firstItemIndex = (pageNumber - 1) * itemsPerPage;
+            IQueryable<Clients> clientsQuery = db.context.Clients.OrderBy(x => x.Clients_Lastname);
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                clientsQuery = clientsQuery.Where(client =>
+                    client.Clients_Lastname.Contains(searchQuery) ||
+                    client.Clients_Name.Contains(searchQuery) ||
+                    client.Clients_Phone.Contains(searchQuery));
+            }
+
+            var clients = clientsQuery.Skip(firstItemIndex).Take(itemsPerPage).ToList();
+
+            MainPatientsStackPanel.Children.Clear();
+
+            foreach (var client in clients)
+            {
+                Button button = new Button
+                {
+                    Name = $"Button{client.Clients_Id}",
+                    Content = $" №{client.Clients_Id}. {client.Clients_Lastname} {client.Clients_Name}",
+                    Style = FindResource("PatientsButton") as Style,
+                    Margin = new Thickness(0, 10, 0, 0)
+                };
+
+                button.Click += Button_Click;
+                MainPatientsStackPanel.Children.Add(button);
+            }
+        }
+
+        private void AddPageButtons(string searchQuery = "")
+        {
+            int totalItems = string.IsNullOrEmpty(searchQuery) ? db.context.Clients.Count() : GetMatchingClientsCount(searchQuery);
+            int totalPages = (int)Math.Ceiling((double)totalItems / itemsPerPage);
+
+            for (int i = 1; i <= totalPages; i++)
+            {
+                Button pageButton = new Button
+                {
+                    Content = i.ToString(),
+                    Tag = i,
+                    Margin = new Thickness(0, 0, 10, 0)
+                };
+
+                pageButton.Click += PageButton_Click;
+                paginationStackPanel.Children.Add(pageButton);
+            }
+        }
+
+        private int GetMatchingClientsCount(string searchQuery)
+        {
+            return db.context.Clients
+                .Count(client =>
+                    client.Clients_Lastname.Contains(searchQuery) ||
+                    client.Clients_Name.Contains(searchQuery) ||
+                    client.Clients_Phone.Contains(searchQuery));
+        }
+
+        private void ClearPagination()
+        {
+            paginationStackPanel.Children.Clear();
+        }
+
+        private void PageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int pageNumber = (int)button.Tag;
+            LoadPage(pageNumber, SearchTextBox.Text.Trim());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            int id = int.Parse(button.Name.Substring(6));
+            NavigationService.Navigate(new ViewingPatientPage(id));
         }
     }
 }
